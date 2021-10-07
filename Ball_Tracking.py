a397215e98ecdbe0f9745dd9bbe79b266d303862
RiveraHeitor <heitorrivera@gmail.com>
2021-10-07 15:07:15 -0300

    Primeira versão com medição de distancia

--- Ball_Tracking.py
+++ Ball_Tracking.py
@@ -5,9 +5,13 @@ import keyboard
 import imutils
 from imutils.video import VideoStream
 
+def calc_distancia(frame, centro_b, centro_r):
+    cv2.line(frame, centro_b, centro_r, (0,0,255), 3, 8)
+    return frame
+
 # Função para detectar robos aliados.
 def MascRobos(frame):
-    frame = imutils.resize(frame, width=600) #resize, redefine o tamanho da imagem, usamos esse pq é mais rapido que o cv2.
+    frame = imutils.resize(frame, width=600, height=600) #resize, redefine o tamanho da imagem, usamos esse pq é mais rapido que o cv2.
     blur = cv2.GaussianBlur(frame, (11, 11), 0) #Transforma a imagem para borrada. (Só ímpares)
     hsv = cv2.cvtColor(blur, cv2.COLOR_BGR2HSV) #Aplica na imagem borrada um filtro HSV.
     Cormaxima = np.array([117, 225, 158]) #Shade maximo da bola (laranja), array em HSV.
@@ -25,16 +29,17 @@ def MascRobos(frame):
             center = (int(M["m10"] / M["m00"]), int(M["m01"] / M["m00"])) #calculo doido
             if raio > 10: #condicao para  nao detectar falsos positivos.
                 cv2.circle(frame, center, 5, (0, 0, 255), -1) #desenha o circulo no centro
+                cv2.putText(frame, "Robo aliado", (int(xc), int(yc)), cv2.FONT_HERSHEY_SIMPLEX, 0.7, (0,0,255))
         else:
             center = (0, 0) 
 
 
-    return frame 
+    return frame, center 
 
 # Função para aplicar mascara nas imagens recebinas
 def MascBola(frame):
 
-    frame = imutils.resize(frame, width=600) #resize, redefine o tamanho da imagem, usamos esse pq é mais rapido que o cv2.
+    frame = imutils.resize(frame, width=600, height=600) #resize, redefine o tamanho da imagem, usamos esse pq é mais rapido que o cv2.
     blur = cv2.GaussianBlur(frame, (11, 11), 0) #Transforma a imagem para borrada. (Só ímpares)
     hsv = cv2.cvtColor(blur, cv2.COLOR_BGR2HSV) #Aplica na imagem borrada um filtro HSV.
     Cormaxima = np.array([17, 255, 255]) #Shade maximo da bola (laranja), array em HSV.
@@ -53,9 +58,10 @@ def MascBola(frame):
             if raio > 10: #condicao para  nao detectar falsos positivos.
                 cv2.circle(frame, (int(xc), int(yc)), int(raio), (255,0,0), 3) #desenha o circulo na bola
                 cv2.circle(frame, center, 5, (0, 0, 255), -1) #desenha o circulo no centro
+                cv2.putText(frame, "bola", (int(xc), int(yc)), cv2.FONT_HERSHEY_SIMPLEX, 0.7, (255,0,0))
         else:
             center = (0, 0)
-    return frame
+    return frame, center
 
 
 
@@ -65,10 +71,11 @@ cap = VideoStream(src=0).start()
 t00 = time.time()
 while not keyboard.is_pressed('q'):   # Função de captura da imagem
     frame = cap.read() # Captura da imagem
-    mascB = MascBola(frame)
-    mascR = MascRobos(frame)
-    cv2.imshow('mascBola', mascB) # Exibicao da imagem
-    cv2.imshow('MascRobos', mascR)
+    mascB, centro_b = MascBola(frame)
+    mascR, centro_r = MascRobos(mascB)
+    dist = calc_distancia(mascR, centro_b, centro_r)
+    cv2.imshow('mascBola', dist) # Exibicao da imagem
+    #cv2.imshow('MascRobos', mascR)
     k = cv2.waitKey(5) & 0xFF
     t01 = time.time() # Frame t para calculo do FPS
     print("{} fps, size: {}".format(int(1./(t01 - t00)), frame.shape)) # Printa informacoes sobre a imagem no terminal.
@@ -77,3 +84,6 @@ while not keyboard.is_pressed('q'):   # Função de captura da imagem
         break
 
 cv2.destroyAllWindows()
+
+
+
