907494f2778fcfe7f9ffd5e487877a28e1ec477c
RiveraHeitor <heitorrivera@gmail.com>
2021-10-07 17:40:51 -0300

    Medição de distancia funcional

--- Ball_Tracking.py
+++ Ball_Tracking.py
@@ -5,8 +5,12 @@ import keyboard
 import imutils
 from imutils.video import VideoStream
 
-def calc_distancia(frame, centro_b, centro_r):
+def calc_distancia(frame, centro_b, centro_r, xcentR, ycentR, xcentB, ycentB):
     cv2.line(frame, centro_b, centro_r, (0,0,255), 3, 8)
+    euclid = np.sqrt((xcentB - xcentR)**2 + (ycentB - ycentR)**2)
+    num = round(euclid, 2)
+    dis = str(num)
+    cv2.putText(frame, dis, centro_b, cv2.FONT_HERSHEY_SIMPLEX, 0.7, (0,255,0))
     return frame
 
 # Função para detectar robos aliados.
@@ -21,20 +25,22 @@ def MascRobos(frame):
     mascara = cv2.erode(mascara,None,iterations=3) #aplica efeitos de mascara.
     contorno = cv2.findContours(mascara.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)[-2] #aplica contornos na area detectada.
     center = None
+    xcentro, ycentro = 0 , 0
     if len(contorno) > 0: #só entra no if se o tamanho de contorno > 0
         c = max(contorno, key=cv2.contourArea)
         (xc, yc), raio = cv2.minEnclosingCircle(c) #pega valores para desenhar o circulo
         M = cv2.moments(c) #funcao foda para achar o centro de uma blob
         if M["m00"] != 0: #ocasionalmente m00 pode se tornar 0, assim verificamos.
             center = (int(M["m10"] / M["m00"]), int(M["m01"] / M["m00"])) #calculo doido
+            xcentro, ycentro = (int(M["m10"] / M["m00"]), int(M["m01"] / M["m00"]))
             if raio > 10: #condicao para  nao detectar falsos positivos.
                 cv2.circle(frame, center, 5, (0, 0, 255), -1) #desenha o circulo no centro
                 cv2.putText(frame, "Robo aliado", (int(xc), int(yc)), cv2.FONT_HERSHEY_SIMPLEX, 0.7, (0,0,255))
         else:
             center = (0, 0) 
+            xcentro, ycentro = 0 , 0
 
-
-    return frame, center 
+    return frame, center, xcentro, ycentro 
 
 # Função para aplicar mascara nas imagens recebinas
 def MascBola(frame):
@@ -49,19 +55,22 @@ def MascBola(frame):
     mascara = cv2.erode(mascara,None,iterations=3) #aplica efeitos de mascara.
     contorno = cv2.findContours(mascara.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)[-2] #aplica contornos na area detectada.
     center = None
+    xcentro, ycentro = 0 , 0
     if len(contorno) > 0: #só entra no if se o tamanho de contorno > 0
         c = max(contorno, key=cv2.contourArea)
         (xc, yc), raio = cv2.minEnclosingCircle(c) #pega valores para desenhar o circulo
         M = cv2.moments(c)
         if M["m00"] != 0:
             center = (int(M["m10"] / M["m00"]), int(M["m01"] / M["m00"])) #calculo doido
+            xcentro, ycentro = (int(M["m10"] / M["m00"]), int(M["m01"] / M["m00"]))
             if raio > 10: #condicao para  nao detectar falsos positivos.
                 cv2.circle(frame, (int(xc), int(yc)), int(raio), (255,0,0), 3) #desenha o circulo na bola
                 cv2.circle(frame, center, 5, (0, 0, 255), -1) #desenha o circulo no centro
                 cv2.putText(frame, "bola", (int(xc), int(yc)), cv2.FONT_HERSHEY_SIMPLEX, 0.7, (255,0,0))
         else:
             center = (0, 0)
-    return frame, center
+            xcentro, ycentro = 0 , 0
+    return frame, center, xcentro, ycentro
 
 
 
@@ -71,9 +80,9 @@ cap = VideoStream(src=0).start()
 t00 = time.time()
 while not keyboard.is_pressed('q'):   # Função de captura da imagem
     frame = cap.read() # Captura da imagem
-    mascB, centro_b = MascBola(frame)
-    mascR, centro_r = MascRobos(mascB)
-    dist = calc_distancia(mascR, centro_b, centro_r)
+    mascB, centro_b, xcentR, ycentR = MascBola(frame)
+    mascR, centro_r, xcentB, ycentB = MascRobos(mascB)
+    dist = calc_distancia(mascR, centro_b, centro_r, xcentR, ycentR, xcentB, ycentB)
     cv2.imshow('mascBola', dist) # Exibicao da imagem
     #cv2.imshow('MascRobos', mascR)
     k = cv2.waitKey(5) & 0xFF
