3a2e76b38ed7a9396295fa693116d2cb8885dab2
RiveraHeitor <heitorrivera@gmail.com>
2021-11-20 15:12:14 -0300

    interface melhorada

--- Ball_Tracking.py
+++ Ball_Tracking.py
@@ -1,11 +1,22 @@
 from tkinter import *
+from PIL import ImageTk, Image
 import numpy as np                              
 import cv2
 import time
 import imutils
 from imutils.video import VideoStream
 
+def cantos_arena(frame):
+    blur = cv2.GaussianBlur(frame, (11, 11), 0)
+    hsv = cv2.cvtColor(blur, cv2.COLOR_BGR2HSV)
+    Cormaxima = np.array([0, 0, 100])
+    Corminima = np.array([0, 0, 100])
+    mascara = cv2.inRange(hsv, Corminima, Cormaxima)
+    #branco = cv2.bitwise_and(frame, frame, mask=mascara)
+    return mascara
 
+
+# Função para calcular a distancia entre robôs aliados e a bola 
 def calc_distancia(frame, xcentR, ycentR, xcentB, ycentB):
     cv2.line(frame, (xcentR, ycentR), (xcentB, ycentB), (0,0,255), 3, 8)
     centerR = ((xcentR + 1), (ycentR + 1))
@@ -17,7 +28,7 @@ def calc_distancia(frame, xcentR, ycentR, xcentB, ycentB):
 
 # Função para detectar robos aliados.
 def MascRobos(frame, num, cmin, cmax):
-    frame = imutils.resize(frame, width=600, height=600) #resize, redefine o tamanho da imagem, usamos esse pq é mais rapido que o cv2.
+    
     blur = cv2.GaussianBlur(frame, (11, 11), 0) #Transforma a imagem para borrada. (Só ímpares)
     hsv = cv2.cvtColor(blur, cv2.COLOR_BGR2HSV) #Aplica na imagem borrada um filtro HSV.
     Cormaxima = cmin #Shade maximo da bola (laranja), array em HSV.
@@ -45,7 +56,6 @@ def MascRobos(frame, num, cmin, cmax):
 # Função para aplicar mascara nas imagens recebinas
 def MascBola(frame):
 
-    frame = imutils.resize(frame, width=600, height=600) #resize, redefine o tamanho da imagem, usamos esse pq é mais rapido que o cv2.
     blur = cv2.GaussianBlur(frame, (11, 11), 0) #Transforma a imagem para borrada. (Só ímpares)
     hsv = cv2.cvtColor(blur, cv2.COLOR_BGR2HSV) #Aplica na imagem borrada um filtro HSV.
     Cormaxima = np.array([17, 255, 255]) #Shade maximo da bola (laranja), array em HSV.
@@ -70,10 +80,39 @@ def MascBola(frame):
     return frame, xcentro, ycentro
 
 
-
 cap = VideoStream(src=0).start()
 t00 = time.time()
 
+
+def tela_calibracao():
+    
+    hsv = show_frame()
+    imagem = Image.fromarray(hsv)
+    imgtk = ImageTk.PhotoImage(image=imagem)
+    picker.imgtk = imgtk
+    #picker.after(calibrar)
+    Label(picker, image=imgtk).pack()
+    
+    
+    
+
+def show_frame():
+    global imgtk
+    global frame
+    cmin = np.array([hmin.get(), smin.get(), vmin.get()])
+    cmax = np.array([hmax.get(), smax.get(), vmax.get()])
+    frame = cap.read()
+    frame = imutils.resize(frame, width=600, height=600)
+    frame = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
+    #frame = cv2.inRange(frame, cmin, cmax)
+    imagem = Image.fromarray(frame)
+    imgtk = ImageTk.PhotoImage(image=imagem)
+    picker.imgtk = imgtk
+    foto = Label(picker, image=imgtk)
+    foto.pack()
+    Label(picker, text="imagem aqui").pack()
+    
+   
     
 def iniciar():
     root.destroy()
@@ -123,19 +162,43 @@ Button(root, text="1",width= 15 , command= lambda: h.set('1')).place(x=20, y=20)
 Button(root, text="2",width= 15, command= lambda: h.set('2')).place(x=20, y=45)
 Button(root, text="3",width= 15, command= lambda: h.set('3')).place(x=20, y=70)
 Button(root, text="Iniciar",width= 15, command= iniciar).place(x=325, y=275, anchor= S)
-Button(root, text="cancelar",width= 15, command= cancelar).place(x=325, y=300,anchor= S)
- 
-root.mainloop()
+Button(root, text="cancelar",width= 15, command= cancelar).place(x=325, y=325,anchor= S)
+
+
+#interface do color picker
+
+picker = Toplevel()
+picker.geometry("650x600")
+picker.title("Color Picker")
+picker.iconphoto(False, PhotoImage(file='logo_ieee_icone.png'))
+Scale(picker, from_= 0, to= 255,command= show_frame, variable= hmin, orient= HORIZONTAL).place(x=250, y=15)
+Scale(picker, from_= 0, to= 255,command= show_frame, variable= smin, orient= HORIZONTAL).place(x=250, y=55)
+Scale(picker, from_= 0, to= 255,command= show_frame, variable= vmin, orient= HORIZONTAL).place(x=250, y=95)
+Scale(picker, from_= 0, to= 255, variable= hmax, orient= HORIZONTAL).place(x=470, y=15)
+Scale(picker, from_= 0, to= 255, variable= smax, orient= HORIZONTAL).place(x=470, y=55)
+Scale(picker, from_= 0, to= 255, variable= vmax, orient= HORIZONTAL).place(x=470, y=95)
+show_frame()
+
+mainloop()
+
+
+
+
 cmin = np.array([hmin.get(), smin.get(), vmin.get()])
 cmax = np.array([hmax.get(), smax.get(), vmax.get()])
+
+
 
 
 while h.get() == 1 or h.get() == 2 or h.get() == 3  :   # Função de captura da imagem
     cont = h.get()
     frame = cap.read() # Captura da imagem
+    frame = imutils.resize(frame, width=600, height=600)#resize, redefine o tamanho da imagem, usamos esse pq é mais rapido que o cv2.
+    mascA = cantos_arena(frame)
     mascB, xcentB, ycentB = MascBola(frame)
     mascR, xcentR, ycentR = MascRobos(mascB, cont, cmin, cmax)
     dist = calc_distancia(mascR, xcentR, ycentR, xcentB, ycentB)
+    cv2.imshow('cantos', mascA)
     cv2.imshow('mascBola', dist) # Exibicao da imagem
     #cv2.imshow('MascRobos', mascR)
     k = cv2.waitKey(5) & 0xFF
